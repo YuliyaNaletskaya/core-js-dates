b7c2526f6099bf14fc39e9120c439a352af287f5
Yuliya Naletskaya <naletskaya@gmail.com>
2025-03-24 22:14:03 +0300

    refactor: change one solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -33,14 +33,10 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  const time = new Date(date);
-  const options = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  };
-
-  return new Intl.DateTimeFormat(undefined, options).format(time);
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
